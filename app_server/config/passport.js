1c5cdb8e5f80764fab26a10349b9194a1006f56e
Bunchhieng <Bunchhieng@gmail.com>
2016-04-13 20:55:30 -0400

    Fixed populatDB hang due the api has changed their JSON Structure. The log in and sign up are almost finished.

--- app_server/config/passport.js
+++ app_server/config/passport.js
@@ -2,23 +2,24 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
+require('../models/db');
 
 passport.use(new LocalStrategy({
     usernameField: 'email'
   },
   function(username, password, done) {
     User.findOne({
-      email: username
+      email: username.toLowerCase()
     }, function(err, user) {
       if (err) return done(err);
       if (!user) {
         return done(null, false, {
-          message: 'Incorrect username'
+          msg: 'Incorrect username'
         });
       }
       if (!user.validPassword(password)) {
         return done(null, false, {
-          message: 'Incorrect password'
+          msg: 'Incorrect password'
         });
       }
       return done(null, user);
--- populateDB.js
+++ populateDB.js
@@ -18,8 +18,6 @@ require('./app_server/models/db');
 var URL = 'https://randomuser.me/api/';
 // Number of request to randomuser API
 var NUM = 40;
-// Store all the JSON data from the request.
-var results = [];
 
 /**
  * We need:
@@ -29,51 +27,51 @@ var results = [];
  * - profile: name, age, gender, picture, location, videos, followers, followings
  */
 function generatePeople(r) {
-  var o = {};
-  o._id = r.results[0].user.username;
-  o.username = r.results[0].user.username;
-  o.email = r.results[0].user.email;
-  o.password = r.results[0].user.password;
-  o.profile = {
-    name: capitalizeFirstLetter(r.results[0].user.name.first) + ' ' + capitalizeFirstLetter(r.results[0].user.name.last),
-    gender: r.results[0].user.gender.toUpperCase(),
-    picture: r.results[0].user.picture.medium,
-    location: capitalizeFirstLetter(r.results[0].user.location.city) + ', ' + capitalizeFirstLetter(r.results[0].user.location.state),
-    videos: [
-      'https://www.youtube.com/embed/tR87Jk8ECm0',
-      "https://www.youtube.com/embed/1s9Xs6wEZHc",
-      "https://www.youtube.com/embed/iPeIXDRa2HE",
-      "https://www.youtube.com/embed/rOjHhS5MtvA",
-      "https://www.youtube.com/embed/NHVE_GEBFwM",
-      "https://www.youtube.com/embed/0H7aV1XckCo",
-      "https://www.youtube.com/embed/6h5OgqqSYw4",
-      "https://www.youtube.com/embed/TlI_2nsAxPc",
-      "https://www.youtube.com/embed/l0rQFh-dG7s",
-      "https://www.youtube.com/embed/tIx6_Z5v88k",
-      "https://www.youtube.com/embed/L98SQRHVdEY",
-      "https://www.youtube.com/embed/mEq317YwrQY",
-      "https://www.youtube.com/embed/uj26D-bZ4oo",
-      "https://www.youtube.com/embed/LUCtIAAkPh0",
-      "https://www.youtube.com/embed/j8cADX87-2I",
-      "https://www.youtube.com/embed/sPkhzlIdFAo",
-      "https://www.youtube.com/embed/F3WPGtbUlNA",
-      "https://www.youtube.com/embed/t_aR9CJlrpk",
-      "https://www.youtube.com/embed/Yj4nsZ3o1ag",
-      "https://www.youtube.com/embed/Z2OpSIp_hGg",
-      "https://www.youtube.com/embed/0E040mYpJDw",
-      "https://www.youtube.com/embed/xQlp1fc-mI0",
-      "https://www.youtube.com/embed/L_PS8wZNqrc",
-      "https://www.youtube.com/embed/IOPpyNRBXCM"
-    ],
-    // Random number between 1000000 and 10000
-    followers: Math.floor(Math.random() * (1000000 - 10000 + 1) + 100000),
-    followings: Math.floor(Math.random() * (1000000 - 10000 + 1) + 10000)
-  };
-  // Get a random string with 3 letters
-  o.resetPasswordToken = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-  // Use moment to get current data and make a nice format
-  o.resetPasswordExpires = moment().add(700, 'days').format('MMMM Do YYYY, h:mm:ss a');
-  return o;
+    var o = {};
+    // o._id = r.results[0].user.username;
+    o.username = r.results[0].login.username;
+    o.email = r.results[0].email;
+    o.password = r.results[0].login.password;
+    o.profile = {
+        name: capitalizeFirstLetter(r.results[0].name.first) + ' ' + capitalizeFirstLetter(r.results[0].name.last),
+        gender: r.results[0].gender.toUpperCase(),
+        picture: r.results[0].picture.medium,
+        location: capitalizeFirstLetter(r.results[0].location.city) + ', ' + capitalizeFirstLetter(r.results[0].location.state),
+        videos: [
+            'https://www.youtube.com/embed/tR87Jk8ECm0',
+            "https://www.youtube.com/embed/1s9Xs6wEZHc",
+            "https://www.youtube.com/embed/iPeIXDRa2HE",
+            "https://www.youtube.com/embed/rOjHhS5MtvA",
+            "https://www.youtube.com/embed/NHVE_GEBFwM",
+            "https://www.youtube.com/embed/0H7aV1XckCo",
+            "https://www.youtube.com/embed/6h5OgqqSYw4",
+            "https://www.youtube.com/embed/TlI_2nsAxPc",
+            "https://www.youtube.com/embed/l0rQFh-dG7s",
+            "https://www.youtube.com/embed/tIx6_Z5v88k",
+            "https://www.youtube.com/embed/L98SQRHVdEY",
+            "https://www.youtube.com/embed/mEq317YwrQY",
+            "https://www.youtube.com/embed/uj26D-bZ4oo",
+            "https://www.youtube.com/embed/LUCtIAAkPh0",
+            "https://www.youtube.com/embed/j8cADX87-2I",
+            "https://www.youtube.com/embed/sPkhzlIdFAo",
+            "https://www.youtube.com/embed/F3WPGtbUlNA",
+            "https://www.youtube.com/embed/t_aR9CJlrpk",
+            "https://www.youtube.com/embed/Yj4nsZ3o1ag",
+            "https://www.youtube.com/embed/Z2OpSIp_hGg",
+            "https://www.youtube.com/embed/0E040mYpJDw",
+            "https://www.youtube.com/embed/xQlp1fc-mI0",
+            "https://www.youtube.com/embed/L_PS8wZNqrc",
+            "https://www.youtube.com/embed/IOPpyNRBXCM"
+        ],
+        // Random number between 1000000 and 10000
+        followers: Math.floor(Math.random() * (1000000 - 10000 + 1) + 100000),
+        followings: Math.floor(Math.random() * (1000000 - 10000 + 1) + 10000)
+    };
+    // Get a random string with 3 letters
+    o.resetPasswordToken = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+    // Use moment to get current data and make a nice format
+    o.resetPasswordExpires = moment().add(700, 'days').format('MMMM Do YYYY, h:mm:ss a');
+    return o;
 }
 /**
  * We need to empty the database everytime we populate it to avoid duplicate error
@@ -81,23 +79,23 @@ function generatePeople(r) {
  * @param {Object} db - JSON object
  */
 function emptyThenInsert(db) {
-  UserSchema.remove({}, function(err) {
-    if(err) console.log(err);
-    UserSchema.collection.insert(db, function(err, data) {
-      if (err) console.log(err);
-      UserSchema.collection.find({}, function(err, data) {
+    UserSchema.remove({}, function(err) {
         if (err) console.log(err);
-      });
-      process.exit(1);
+        UserSchema.collection.insert(db, function(err, data) {
+            if (err) console.log(err);
+            UserSchema.collection.find({}, function(err, data) {
+                if (err) console.log(err);
+            });
+            process.exit(1);
+        });
     });
-  });
 }
 
 /**
  * Change the first character to uppercase
  */
 function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 /**
  * Do n requests to ranmdom user api
@@ -106,39 +104,39 @@ function capitalizeFirstLetter(string) {
  * @param {Number} num - Number of request
  */
 function runSequence(url, num) {
-  var cntr = 0;
-  return new Promise(function(resolve, reject) {
+    var cntr = 0;
+    var results = [];
+
     function checkDone(data) {
-      ++cntr;
-      results.push(data);
-      if (cntr < num) {
-        next();
-      } else {
-        resolve(results);
-      }
+        ++cntr;
+        results.push(data);
+        if (cntr < num) {
+            return next();
+        } else {
+            return results;
+        }
     }
 
     function next() {
-      got(url).then(response => {
-        // console.log(response.body);
-        checkDone(response.body);
-      }).catch(error => {
-        console.log(error.response.body);
-        checkDone(null);
-      });
+        return got(url).then(response => {
+            return checkDone(response.body);
+        }).catch(error => {
+            return checkDone(null);
+        });
     }
-    next();
-  });
+
+    return next();
 }
 
 // Using Promise to get 10 results
 runSequence(URL, NUM).then(function(results) {
-  console.log(results.length + " documents has been inserted to the database.");
-  var db = [];
-  for (var i = 0; i < NUM; i++) {
-    var result = JSON.parse(results[i]);
-    var d = generatePeople(result);
-    db.push(d);
-  }
-  emptyThenInsert(db);
+    var db = [];
+    for (var i = 0; i < results.length; i++) {
+        var result = JSON.parse(results[i]);
+        var d = generatePeople(result);
+        console.log(d);
+        db.push(d);
+    }
+    emptyThenInsert(db);
+    console.log(results.length + " documents has been inserted to the database.");
 });
