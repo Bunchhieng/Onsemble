7db91d74df45b07a346ccea1624469b72c6112fb
Santiago Paredes <santiago.paredes2012@gmail.com>
2016-04-22 15:20:28 -0400

    Huge Overhaul -- Major Changes

--- public/assets/js/discover.js
+++ public/assets/js/discover.js
@@ -19,19 +19,114 @@ jQuery(function($) {
 		$('#navigation').fadeOut();
 	});
 
-	var slider = $('#page-slider .carousel-inner').find('.item');
-	$('#page-slider').on('slid.bs.carousel', function () {
-		var curIndex 	= slider.filter('.active').index();
-		var menuItems 	= $('.main-nav ul').find('li');
-		menuItems.removeClass('active').eq(curIndex).addClass('active');
-	});
+	// List of discover URL's
+	var boston_urls = ['https://www.youtube.com/embed/V0Gzsdmp3Yc', 'https://www.youtube.com/embed/1s9Xs6wEZHc', 'https://www.youtube.com/embed/CI4TMwFqEgA', 'https://www.youtube.com/embed/NHVE_GEBFwM', 'https://www.youtube.com/embed/0H7aV1XckCo', 'https://www.youtube.com/embed/6h5OgqqSYw4', 'https://www.youtube.com/embed/l0rQFh-dG7s', 'https://www.youtube.com/embed/9c1i7id2zdE', 'https://www.youtube.com/embed/tIx6_Z5v88k'];
+	var lowell_urls = ['https://www.youtube.com/embed/J079e95caB4', 'https://www.youtube.com/embed/jASmF2nsBEI', 'https://www.youtube.com/embed/CnWCF3ND09I', 'https://www.youtube.com/embed/XO1bQ5JSYcY', 'https://www.youtube.com/embed/T-oJCNiiLCQ', 'https://www.youtube.com/embed/S2UfGtHCH2s']
 
-	var slider2 = $('#page-slider2 .carousel-inner').find('.item');
-	$('#page-slider2').on('slid.bs.carousel', function () {
-		var curIndex 	= slider.filter('.active').index();
-		var menuItems 	= $('.main-nav ul').find('li');
-		menuItems.removeClass('active').eq(curIndex).addClass('active');
-	});
+	// Dict of streams
+	var streams = {'boston': boston_urls, 'lowell': lowell_urls};
+
+	function setYouTubeTitle(streamName, VIDEOID, id) {
+		console.log('here - title');
+		console.log(streamName+'title'+id);
+	    requestInfo(VIDEOID, function(body) {
+	        document.getElementById(streamName+'title'+id).innerHTML = JSON.stringify(body.items[0].snippet.title);
+	    });
+	}
+
+	function setYouTubeName(streamName, VIDEOID, id) {
+		console.log('here - name');
+		console.log(streamName+'name'+id);
+        requestInfo(VIDEOID, function(body) {
+            document.getElementById(streamName+'name'+id).innerHTML = JSON.stringify(body.items[0].snippet.channelTitle);
+        });
+	}
+	// Need to limit how many layers of a stream are made based on how many
+	// videos are in list
+	function buildStream(streamName, url_list) {
+
+		var html = [];
+		var i = 0;
+
+		buildCarousel(streamName);
+		buildLayer(streamName, 'active');
+		buildLayer(streamName, '');
+		buildLayer(streamName, '');
+		buildDataSliders(streamName);
+
+		console.log(html.join(""));
+		return html.join("");
+
+		// Builder functions
+		function buildCarousel(streamName) {
+			html.push('<div id="'+streamName+'" class="carousel slide">');
+			html.push('<div class="carousel-inner">');
+		}
+
+		function buildIFrame(streamName, id) {
+			html.push('<li class="col-sm-4">');
+			html.push('<div class="video-pad">');
+			html.push('<div id="'+streamName+'youtube_video'+id+'"></div>');
+			html.push('<div class="title-box">');
+			html.push('<h4 id="'+streamName+'name'+id+'"></h4>'); // artist name
+			html.push('<p id="'+streamName+'title'+id+'"></p>');
+			html.push('</div>');
+			html.push('</div>');
+			html.push('</li>');
+		}
+
+		function buildLayer(streamName, active) {
+			html.push('<div class="item '+active+'">');
+			html.push('<ul class="thumbnails">');
+			for (var j = 0; j < 3; j++) {			// this is expecting to display groups of 3
+				buildIFrame(streamName, i);
+				i++;
+			}
+			html.push('</ul>');
+			html.push('</div>');
+		}
+
+		function buildDataSliders(streamName) {
+			html.push('</div>');	// Close layers
+			html.push('<ul class="control-box pager">');
+			html.push('<li><a data-slide="prev" href="#'+streamName+'"><i class="fa fa-arrow-left fa-3x"></i></a></li>');
+			html.push('<li><a data-slide="next" href="#'+streamName+'"><i class="fa fa-arrow-right fa-3x"></i></a></li>');
+			html.push('</ul>');		// Close stream
+			html.push('</div>');	// Close stream
+		}
+	}
+
+	// http://stackoverflow.com/questions/6164507/change-the-content-of-a-div-based-on-selection-from-drop-down-menu
+    function replaceContentInContainer(matchClass, content) {
+	    var elems = document.getElementsByTagName('*'), i;
+	    for (i in elems) {
+	        if((' ' + elems[i].className + ' ').indexOf(' ' + matchClass + ' ')
+	                > -1) {
+	            elems[i].innerHTML = content;
+	        }
+	    }
+	};
+
+	// get the names/types of streams
+	var keys = Object.keys(streams);
+	// loop through the stream names, build them, and place their content on the page
+	for (var i = 0; i < keys.length; i++) {
+		var key = keys[i];
+		tempStream = buildStream(key, streams[key]);
+		replaceContentInContainer(key, tempStream);
+	}
+
+	// loop through stream names and place appropriate videos where they belong
+    for (var i = 0; i < keys.length; i++) {
+    	var key = keys[i];
+    	for (var j = 0; j < streams[key].length; j++) {
+	        var VIDEOID = youtube_parser(streams[key][j]);
+	        setYouTubeTitle(key, VIDEOID, j);
+	        setYouTubeName(key, VIDEOID, j);
+	        imgFrame(key+"youtube_video"+j, VIDEOID, 340, 240);
+	        console.log('here - placevideo');
+	    }
+	}
 
 	//Contact Form
 	var form = $('#contact-form');
@@ -48,4 +143,33 @@ jQuery(function($) {
 		});
 	});
 
+
 });
+
+	// var slider = $('#page-slider .carousel-inner').find('.item');
+	// $('#page-slider').on('slid.bs.carousel', function () {
+	// 	var curIndex 	= slider.filter('.active').index();
+	// 	var menuItems 	= $('.main-nav ul').find('li');
+	// 	menuItems.removeClass('active').eq(curIndex).addClass('active');
+	// });
+
+	// var slider2 = $('#page-slider2 .carousel-inner').find('.item');
+	// $('#page-slider2').on('slid.bs.carousel', function () {
+	// 	var curIndex 	= slider.filter('.active').index();
+	// 	var menuItems 	= $('.main-nav ul').find('li');
+	// 	menuItems.removeClass('active').eq(curIndex).addClass('active');
+	// });
+
+
+	// http://stackoverflow.com/questions/6164507/change-the-content-of-a-div-based-on-selection-from-drop-down-menu
+	// $('select.div-toggle').change(function(){
+	//   var target = $(this).data('target');
+	//   var show = $("option:selected", this).data('show');
+	//   $(target).children().addClass('hide');
+	//   $(show).removeClass('hide');
+	// });
+
+	// var myCarousel = $('#myCarousel').html();
+	// var test = $('.boston').html(myCarousel)
+	// var myCarousel2 = $('#myCarousel2').html();
+	// var test2 = $('.lowell').html(myCarousel)
--- public/assets/js/youtubeVideos.js
+++ public/assets/js/youtubeVideos.js
@@ -1,113 +1,143 @@
+/*****************************************************************************
+ * Notice some function might not be used.
+ ****************************************************************************/
 
-/* This function require a YouTube url and return a YouTube video ID
- */
+/* This function require a YouTube url and return a YouTube video ID */
 function youtube_parser(url) {
     var match = url.match(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/);
     return (match && match[7].length == 11) ? match[7] : false;
 }
 
-/* This function requires a YouTube video ID and return an embeded YouTube url.
- * With below conditions:
- * autoplay=1 - autoplay the video once the video is buffea
- * start=0&end=15 - show the first 15 seconds of the embed video
- * showinfo=0 - show NO information such as title
- * controls=0 - show NO player controls
- * disablekb=1 - disable keyboard
- * rel=0 - show no relevance video after video is done
- * However, disabling showinfo and controls will result in showing a YouTube
- * logo on the lower right corner of the video. This logo can't not be remove
- * otherwise it will violate YouTube terms
+/* It will replace given div to iframe.
+ * Two things noted:
+ *    It will REPLACE the given div with iframe.
+ *    It ONLY works with id.
  */
-function youtubeEmbededURL(videoID) {
-    return "http://www.youtube.com/embed/" + videoID + "?autoplay=1&start=21&end=37&showinfo=0&controls=0&disablekb=1&rel=0";
+function youtubeDiv(parent_div_ID, div_ID, VIDEOID, width, height, start_time, end_time) {
+    // create and replace div_ID to iframe with parameters
+    var player = new YT.Player(div_ID, {
+        width: width,
+        height: height,
+        videoId: VIDEOID,
+        playerVars: { 
+            autoplay: 1,
+            start: start_time,
+            end: end_time,
+            showinfo: 0,
+            controls: 0,
+            disablekb: 1,
+            rel: 0
+        },
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+    // When player is not ready, it will know if the mouse
+    // is inside the frame or not.
+    var OutOfFrameBeforeReady = true;
+    // on hover
+    $(parent_div_ID).mouseover(function() {
+        OutOfFrameBeforeReady = false;
+    });
+    // on out of element
+    $(parent_div_ID).mouseout(function() {
+        OutOfFrameBeforeReady = true;
+    });
+
+    // When player is ready, the API will call this function
+    // pauseVideo() or playVideo() will only works inside these functions
+    function onPlayerReady(event) {
+        // console.log("onPlayerReady");
+
+        player.playVideo();
+        // if the mouse is out of frame before player is ready.
+        if(OutOfFrameBeforeReady) {
+            // load in the first 100 milliseconds of video instead of a
+            // continuous loading circle icon
+            setTimeout(function() {
+                player.pauseVideo();
+            }, 100);
+        }
+
+        // on hover
+        $(parent_div_ID).mouseover(function() {
+            // console.log("in2");
+            player.playVideo();
+        });
+        // on out of element
+        $(parent_div_ID).mouseout(function() {
+            // console.log("out2");
+            player.pauseVideo();
+        });
+    }
 }
 
-/* Add youtube to given div id
- * notice id with hash tag
+/* imgFrame is the image before replace the div into iframe.
+ * For now, the thumbnail is the image.
+ * Also, only id are allowed.
  */
-
-function youtubeDiv(div_ID, VIDEOID, width, height) {
-    // getting youtube url
-    var url = youtubeEmbededURL(VIDEOID);
-
-    // object
-    var object = document.createElement('iframe');
-
-    object.id = VIDEOID;
-    object.src = url;
-    object.width = width;
-    object.height = height;
-    object.allowfullscreen;
-
-    // appending the object tag to required div
-    $(div_ID).append(object);
-}
-
 function imgFrame(div_ID, VIDEOID, width, height) {
-    //mine
-    var iframe = $('iframe:first');
-    var iframe_src = iframe.attr('src');
-    var youtube_video_id = VIDEOID;
-    //end
-    //if (youtube_video_id.length == 11) {
-    var video_thumbnail = 'http://img.youtube.com/vi/' + youtube_video_id + '/0.jpg';
-    //$('body').append(video_thumbnail);
-    //}
+    start = 32;
+    end = 47;
+
     var object = document.createElement('img');
 
     object.id = VIDEOID;
-    object.src = video_thumbnail;
+    object.src = "http://img.youtube.com/vi/" + VIDEOID + "/0.jpg";
     object.width = width;
     object.height = height;
-    // hover on image to play video
-    object.onclick = function() {
-        object.remove();
-        youtubeDiv(div_ID, VIDEOID, width, height);
-    };
 
-    $(div_ID).append(object);
+    // hover play required a wrap <div> because
+    // before the player is ready, it will unable to detect hover
+    $("#"+div_ID).wrap("<div id='iframe_"+ div_ID +"'></div>");
+    // hover depend on this css
+    $("#"+div_ID).css("display", "flex");
+    $("#iframe_"+div_ID).css("display", "inline-block");
+
+    // debugging code
+    // $("#"+div_ID).css("border", "solid blue");
+    // $("#iframe_"+div_ID).css("border", "solid red");
+
+    // append image to div
+    $("#"+div_ID).append(object);
+
+    // hover on image to play video
+    object.onmouseover = function() {
+        object.remove();
+        youtubeDiv("#iframe_"+div_ID, div_ID, VIDEOID, width, height, start, end);
+    };
 }
 
-/* Request the url */
-function retrInfoFromVideoID(VIDEOID) {
+/* Return the url of youtube informations with this videoId */
+function retrInfoUrlFromVideoID(VIDEOID) {
     // url for retreiving data
     var url = "https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet"
 
-    var vid = "&id=" + VIDEOID;
+    var vId = "&id=" + VIDEOID;
     // required API key for v3: AIzaSyCSF8SnzxmLgor11z8dtzaPLmXt1l5WX-k
     // from takyiulo@gmail.com
     var API_KEY = "&key=" + "AIzaSyCSF8SnzxmLgor11z8dtzaPLmXt1l5WX-k";
 
-    return url + vid + API_KEY;
+    return url + vId + API_KEY;
 }
 
-/* Request the info, client side
- * This is what it looks like:
+/* Request the informations of a youtube video.
+ * An Example of a JSON structure for informations:
  * https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&id=4Y4YSpF6d6w&key=AIzaSyCSF8SnzxmLgor11z8dtzaPLmXt1l5WX-k
  */
-function requestInfo(VIDEOID) {
+function requestInfo(VIDEOID, callback) {
     var xmlhttp = new XMLHttpRequest();
-    var url = retrInfoFromVideoID(VIDEOID);
+    var url = retrInfoUrlFromVideoID(VIDEOID);
 
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            var myArr = JSON.parse(xmlhttp.responseText);
-            console.log("Request data" + JSON.stringify(myArr));
+            var body = JSON.parse(xmlhttp.responseText);
+            callback(body);
+        } else {
+            console.log(xmlhttp.statusText, xmlhttp.status);
         }
     };
 
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
-}
-
-/* Request the info, server side 
- * npm install request 
- */
-function RrequestInfo(VIDEOID) {
-    var request = require('request');
-    request(retrInfoFromVideoID(VIDEOID), function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body) // Show the HTML for the Google homepage.
-        }
-    });
 }
