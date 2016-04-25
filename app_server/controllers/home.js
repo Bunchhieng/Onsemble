98ecba8f711f79eec0e75934d9da44a150bca714
Justin Nguyen <justin_nguyen@student.uml.edu>
2016-04-25 18:45:34 -0400

    overhauled stage

--- app_server/controllers/home.js
+++ app_server/controllers/home.js
@@ -24,6 +24,23 @@ module.exports.stage = function(req, res) {
     })
 }
 
+module.exports.stage2 = function(req, res) {
+    UserSchema.find({}).limit(1).exec(function(err, data) {
+        console.log(data);
+        if (err) console.log(err);
+        //if (!data) {
+        //    req.flash('errors', {
+        //        msg: 'Invalid user in the database.'
+        //    });
+        //    return res.redirect('/');
+        //}
+        res.render('stage', {
+            user: data,
+            title: "Stage"
+        });
+    })
+}
+
 module.exports.discover = function(req, res) {
   UserSchema.find({}).limit(1).exec(function(err, data) {
       console.log(data);
---
+++ public/assets/js/stage.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $('.scroll-down').localScroll({duration:800});
+ })
